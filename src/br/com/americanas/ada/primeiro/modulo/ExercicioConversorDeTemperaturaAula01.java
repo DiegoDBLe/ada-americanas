e480f4561bfda75cc1d713e7e91a1740f0854528
Diego Dantas <deigo_batista@hotmail.com>
2022-12-03 15:45:18 -0300

    Exerccios aula01

--- src/br/com/americanas/ada/primeiro/modulo/ExercicioConversorDeTemperaturaAula01.java
+++ src/br/com/americanas/ada/primeiro/modulo/ExercicioConversorDeTemperaturaAula01.java
@@ -1,7 +1,23 @@
 package br.com.americanas.ada.primeiro.modulo;
 
+import java.util.Scanner;
+
+// - Criar um programa em java que converta a temperatura de Fahrenheit para graus Celsius (clássico):
 public class ExercicioConversorDeTemperaturaAula01 {
 	
-	
-
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Informe a temperatura em Celsius: ");
+		
+		float celsius = entrada.nextFloat();
+		float fahrenheit = (9 * celsius + 160) /5;
+		float kelvin = celsius + 273; 
+		
+		System.out.println(celsius + "° celsius");
+		System.out.println(fahrenheit + " fahrenheit");
+		System.out.println(kelvin + " kelvin");
+		
+		entrada.close();
+	}
 }
